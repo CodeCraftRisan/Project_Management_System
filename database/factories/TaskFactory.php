502b3ff5c81afdd7c30d2a6a272dced58e82c11b
CodeCraftRisan <ahmedrisan01712@gmail.com>
2025-03-01 08:47:53 +0600

    Edit section error solved and photo upload successfully added in projects

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -17,15 +17,13 @@ class TaskFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->sentence(),
-            'description' => fake()->realText(),
-            'due_date' => fake()->dateTimeBetween('now', '+1 year'),
-            'status' => fake()
-                ->randomElement(['pending', 'in_progress', 'completed']),
-            'priority' => fake()
-                ->randomElement(['low', 'medium', 'high']),
-            'image_path' => fake()->imageUrl(),
-            'assigned_user_id' => fake()->randomElement([1, 2]),
+            'name' => $this->faker->sentence(),
+            'description' => $this->faker->realText(),
+            'due_date' => $this->faker->dateTimeBetween('now', '+1 year'),
+            'status' => $this->faker->randomElement(['pending', 'in_progress', 'completed']),
+            'priority' => $this->faker->randomElement(['low', 'medium', 'high']),
+            'image_path' => $this->faker->imageUrl(),
+            'assigned_user_id' => $this->faker->randomElement([1, 2]),
             'created_by' => 1,
             'updated_by' => 1,
             'created_at' => time(),
